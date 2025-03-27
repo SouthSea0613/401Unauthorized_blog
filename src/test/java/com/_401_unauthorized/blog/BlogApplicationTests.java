225bd3b647285dc54c1773ab1343d7bfa67ced1a
South_Sea13 <tksz0613@gmail.com>
2025-03-27 15:28:22 +0900

    Initial commit

---
+++ src/test/java/com/_401_unauthorized/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com._401_unauthorized.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
